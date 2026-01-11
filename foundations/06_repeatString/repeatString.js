0ff3263fd73721e5fc169f582a51ecac27a02166
giacom98 <251912547+giacom98@users.noreply.github.com>
2026-01-11 17:17:40 +0100

    js 06 exercise solution

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(text, repetition) {
+    if (repetition < 0) return "ERROR"
 
+    let string = ""
+    for (let i=0; i < repetition; i++) {
+        string += text
+    }
+    
+    return string
 };
 
 // Do not edit below this line
