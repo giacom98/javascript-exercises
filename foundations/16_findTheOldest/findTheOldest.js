1f152d1f2f12614497d0bcff6862c390a3762525
giacom98 <251912547+giacom98@users.noreply.github.com>
2026-01-25 20:09:11 +0100

    js 16 exercise solution

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people, refYear = new Date().getFullYear()) {
+    if (!Array.isArray(people) || people.length === 0) return null
+
+    const age = (p) => {
+        const birth = p.yearOfBirth
+        const death = (p.yearOfDeath ?? refYear)
+        return death - birth
+    }
+
+    return people.reduce((older, p) => {
+        return age(p) > age(older) ? p : older
+    })
 
 };
 
