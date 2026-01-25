e0ed32111358c0a0ba7b2c84927b6b23679365db
giacom98 <251912547+giacom98@users.noreply.github.com>
2026-01-25 19:48:44 +0100

    js 14 exercise solution

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let count
+    if (typeof n !== 'number') {
+        count = parseInt(n)
+    } else {
+        count = n
+    }
 
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    let a = 0
+    let b = 1
+
+    for (let i = 0; i < count; i++) {
+        [a,b] = [b, a +b];
+    }
+
+    return a
 };
 
 // Do not edit below this line
